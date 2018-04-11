a82d996346e56e72bf4372e8ef22f3aed2cdcf2d
xcxz1994 <981877654@qq.com>
2018-04-10 22:55:30 -0700

    First Test Pass

--- testsuits/baidu_search.py
+++ testsuits/baidu_search.py
@@ -2,7 +2,7 @@
 import time
 import unittest
 from framework.browser_engine import BrowserEngine
-
+from UrsalinkTestWeb.baidu_homepage import HomePage
 
 class BaiduSearch(unittest.TestCase):
 
@@ -28,11 +28,17 @@ class BaiduSearch(unittest.TestCase):
         这里一定要test开头，把测试逻辑代码封装到一个test开头的方法里。
         :return:
         """
-        self.driver.find_element_by_id('kw').send_keys('selenium')
-        self.driver.find_element_by_id('su').click()            #!!!!!!!!!!!
-        time.sleep(5)
+        homepage = HomePage(self.driver)
+        homepage.type_search('selenium')  # 调用页面对象中的方法
+        homepage.send_submit_btn()  # 调用页面对象类中的点击搜索按钮方法
+        time.sleep(2)
+        homepage.get_windows_img()  # 调用基类截图方法
+       # self.driver.find_element_by_id('kw').send_keys('selenium')
+       #self.driver.find_element_by_id('su').click()            #!!!!!!!!!!!
+       # time.sleep(5)
         try:
-            assert 'selenium' in self.driver.title
+            #assert 'selenium' in self.driver.title
+            assert 'selenium' in homepage.get_page_title()
             print ('Test Pass.')
         except Exception as e:
             print ('Test Fail.', format(e))
